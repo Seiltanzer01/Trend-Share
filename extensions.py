91ece917f8af2c9b6f7a81161c9c621241768bcd
Seiltanzer01 <tizinallergia@gmail.com>
2024-10-27 01:02:47 +0300

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+# extensions.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+=======
+# extensions.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+>>>>>>> 0b9f64ca081fba9f10cf43416edc1ce283e060b4
